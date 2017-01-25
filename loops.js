1ce10331443aef916baf13df68f81d6dedc01072
Cynthia Rittenbach <crittenbach@gmail.com>
2017-01-25 06:16:24 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,23 @@
+function forLoop(array) {
+    for (let i = 0; i < 25; i++) {
+        console.log(`I am ${i} strangle loops.`);
+    }}
+
+    function whileLoop(numb) {
+        while(numb>0) {
+          console.log(--numb);
+        }
+        console.log("done");
+    }
+
+    function doWhileLoop(array) {
+    function maybeTrue() {
+        return Math.random() >= 0.5
+}
+    do {
+        array.pop();
+    } while (maybeTrue());
+
+      console.log(array); }
+
+done()
